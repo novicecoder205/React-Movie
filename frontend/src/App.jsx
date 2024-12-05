467603097b9cde1e469488db4ab251506b2e9b9e
novicecoder205 <warpick2000@gmail.com>
2024-12-05 19:41:17 +0700

    7:41pm 12/05/2024

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import MovieCard from './components/MovieCard';
+
 
 function App() {
   const movieNumber =1;
   return (
     <>
-      {movieNumber === 1 && <MovieCard movie={{title: "Gold Rush Update", release_date:"2008"}}/>}
+      
     </>
   );
 }
