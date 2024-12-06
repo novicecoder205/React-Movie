96ddc6ade683092f201bb6fd09c1b44265701705
novicecoder205 <warpick2000@gmail.com>
2024-12-06 09:07:34 +0700

    9:07am 12/06/2024

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,8 @@
+import MovieCard from "../components/MovieCard"
 function Home(){
     const movies = [
         {id: 1, title: "Australian Christmas", release_date: "2010"},
-        {id: 2, title: "The Hatless Update", release_date: "2011"},
+        {id: 2, title: "The Hatless", release_date: "2011"},
         {id: 3, title: "Pyromania" ,release_date: "2012"},
         {id: 4, title: "Mann vs. Machine", release_date: "2012"},
         {id: 5, title: "Scream Fortress", release_date: "2012"},
@@ -9,7 +10,16 @@ function Home(){
         {id: 7, title: "The Two Cities" ,release_date: "2013"},
         {id: 8, title: "Swissmas" ,release_date: "2013"},
         {id: 9, title: "Love & War" ,release_date: "2014"},
-        {id: 3, title: "End Of The Line" ,release_date: "2014"},
+        {id: 10, title: "End Of The Line" ,release_date: "2014"},
+        {id: 11, title: "Gun Mettle" ,release_date: "2015"},
+        {id: 12, title: "Invasion" ,release_date: "2015"},
+        {id: 13, title: "Tought Break" ,release_date: "2015"},
+        {id: 13, title: "The Meet Your Match" ,release_date: "2016"},
+        {id: 13, title: "The Jungle Inferno" ,release_date: "2017"},
     ]
-    return <div className ="home"></div>
+    return <div className ="home">
+        <div className="movies-grid">
+            {movies.map(movie =><MovieCard movie={movie} key={movie.id}/>)}
+        </div>
+    </div>
 }
