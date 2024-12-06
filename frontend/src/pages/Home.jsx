07f631db671f7f484f9ece8cdaa06de6b2992237
novicecoder205 <warpick2000@gmail.com>
2024-12-06 08:47:17 +0700

    8:47 12/06/2024

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,15 @@
 function Home(){
-    
+    const movies = [
+        {id: 1, title: "Australian Christmas", release_date: "2010"},
+        {id: 2, title: "The Hatless Update", release_date: "2011"},
+        {id: 3, title: "Pyromania" ,release_date: "2012"},
+        {id: 4, title: "Mann vs. Machine", release_date: "2012"},
+        {id: 5, title: "Scream Fortress", release_date: "2012"},
+        {id: 6, title: "Robotic Boogaloo", release_date: "2013"},
+        {id: 7, title: "The Two Cities" ,release_date: "2013"},
+        {id: 8, title: "Swissmas" ,release_date: "2013"},
+        {id: 9, title: "Love & War" ,release_date: "2014"},
+        {id: 3, title: "End Of The Line" ,release_date: "2014"},
+    ]
+    return <div className ="home"></div>
 }
